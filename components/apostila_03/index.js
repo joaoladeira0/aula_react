a958032ccc06c6762355f7cf084aa7500b92cbf0
joaoladeira0 <124743276+joaoladeira0@users.noreply.github.com>
2023-05-09 20:48:56 -0300

    09.05.23

--- components/apostila_03/index.js
+++ components/apostila_03/index.js
@@ -1,11 +1,19 @@
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 export default function apostila03() {
   return (
     <View style={styles.container}>
-      <Text>Exemplo Apostila 3</Text>
+      <Text style={styles.paragraph}>Exemplo Apostila 3</Text>
+
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={() => alert('MAIQUE PINTO')}
+      >
+        <Text style={styles.textButton}>Hello World!</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
