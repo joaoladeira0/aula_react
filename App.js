c5e7a7810f4d57dd5b77cf7b33485a812afdc981
joaoladeira0 <124743276+joaoladeira0@users.noreply.github.com>
2023-05-18 20:41:09 -0300

    18.05

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Apostila03A from './components/apostila_03_avancado/index';
+import Apostila04 from './components/apostila_04/index.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Apostila03A />
+      <Apostila04 />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ components/apostila_04/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Apostila04() {
+    return(
+        <View style={styles.container}>
+            <Text>Exemplo 04</Text>
+        </View>
+    );
+}
+
+export default Apostila04;
