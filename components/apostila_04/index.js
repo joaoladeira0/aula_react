ad7b08b74fe7ccac453fbf37f5640579570dd750
joaoladeira0 <124743276+joaoladeira0@users.noreply.github.com>
2023-06-01 20:50:09 -0300

    01.06.2023

--- components/apostila_04/index.js
+++ components/apostila_04/index.js
@@ -1,12 +1,41 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 function Apostila04() {
+
+    const [entrada, setEntrada] = useState('');
+    const [mensagem, setMensagem] =useState('Aqui aparece o texto digitado!');
+
+    function apresentaMensagem() {
+        setMensagem(entrada);
+        setEntrada('');
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.texto}>Exemplo 04</Text>
+
+            <Text 
+                style={styles.txtSaida}
+            >
+                {mensagem}
+            </Text>
+            
+            <TextInput 
+                style={styles.txtEntrada}
+                onChangeText={(inputEntrada) => setEntrada(inputEntrada)}
+                
+            />
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => apresentaMensagem}
+            >
+
+            <Text style={styles.textButton}>Mostrar valor digitado</Text>
+            </TouchableOpacity>
         </View>
     );
 }
